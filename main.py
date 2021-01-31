726ea1101fedd0f7103219d013186a287299e23b
TakisAngelides <54171883+TakisAngelides@users.noreply.github.com>
2021-01-31 13:33:01 +0000

    Add files via upload
    V2

--- main.py
+++ main.py
@@ -118,52 +118,55 @@ def rotate(S, R):
     return [R*v*~R for v in S]
 
 def multivec_to_vec(a):
-    return np.array(a[1],a[2],a[3])
+    return np.array([a[1], a[2], a[3]])
 
 def energy(m,p):
     p = multivec_to_vec(p)
     return np.sqrt(m**2 + np.linalg.norm(p)**2)
+
+# TODO: Incorporate permutations in the algorithm
+
+# For distinct masses between all particles - since permutations are not checked
 
 R1 = e1*e3  # Rotates -p and -q back to themselves
 
 chiral_states = []
 non_chiral_states = []
 
-# For distinct masses between all particles
-for _ in range(3):
+# for _ in range(3):
+#
+#     mp,mq,ma,mb,mc,md = randint(0,10), randint(0,10), randint(0,10), randint(0,10), randint(0,10), randint(0,10)
+#     p = uniform(-10, 10)*e3
+#     q = -p
+#     a = uniform(-10, 10)*e1 + uniform(-10, 10)*e2 + uniform(-10, 10)*e3
+#     b = uniform(-10, 10)*e1 + uniform(-10, 10)*e2 + uniform(-10, 10)*e3
+#     c = uniform(-10, 10)*e1 + uniform(-10, 10)*e2 + uniform(-10, 10)*e3
+#     d = uniform(-10, 10)*e1 + uniform(-10, 10)*e2 + uniform(-10, 10)*e3
+#     Ep,Eq,Ea,Eb,Ec,Ed = energy(mp,p), energy(mq,q), energy(ma,a), energy(mb,b), energy(mc,c), energy(md,d)
+#     E = [Ep,Eq,Ea,Eb,Ec,Ed]
+#
+#     S = [p, q, a, b, c, d]
+#
+#     alpha = arctan(abs(S[2][2])/abs(S[2][1]))  # Angle the vector 'a' makes in the e1-e2 plane
+#
+#     R2 = e**(alpha*(e1*e2))  # Rotates R1(-a)R1' back to 'a'
+#
+#     if S[2][2]*S[2][1] > 0:
+#         R2 = e**(alpha*(e2*e1))  # Rotate in the opposite direction if 'a' lives in the 1st or 3rd quadrant
+#
+#     S1 = parity(S)
+#     S2 = rotate(S1, R1)
+#     S3 = rotate(S2, R2)
+#     final = S3
+#
+#     if S == final:
+#         non_chiral_states.append([S,E])
+#         print(S)
+#         print(final)
+#     else:
+#         chiral_states.append([S,E])
 
-    mp,mq,ma,mb,mc,md = randint(0,10), randint(0,10), randint(0,10), randint(0,10), randint(0,10), randint(0,10)
-    p = uniform(-10, 10)*e3
-    q = -p
-    a = uniform(-10, 10)*e1 + uniform(-10, 10)*e2 + uniform(-10, 10)*e3
-    b = uniform(-10, 10)*e1 + uniform(-10, 10)*e2 + uniform(-10, 10)*e3
-    c = uniform(-10, 10)*e1 + uniform(-10, 10)*e2 + uniform(-10, 10)*e3
-    d = uniform(-10, 10)*e1 + uniform(-10, 10)*e2 + uniform(-10, 10)*e3
-    Ep,Eq,Ea,Eb,Ec,Ed = energy(mp,p), energy(mq,q), energy(ma,a), energy(mb,b), energy(mc,c), energy(md,d)
-    E = [Ep,Eq,Ea,Eb,Ec,Ed]
-
-    S = [p, q, a, b, c, d]
-
-    alpha = arctan(abs(S[2][2])/abs(S[2][1]))  # Angle the vector 'a' makes in the e1-e2 plane
-
-    R2 = e**(alpha*(e1*e2))  # Rotates R1(-a)R1' back to 'a'
-
-    if S[2][2]*S[2][1] > 0:
-        R2 = e**(alpha*(e2*e1))  # Rotate in the opposite direction if 'a' lives in the 1st or 3rd quadrant
-
-    S1 = parity(S)
-    S2 = rotate(S1, R1)
-    S3 = rotate(S2, R2)
-    final = S3
-
-    if S == final:
-        non_chiral_states.append([S,E])
-        print(S)
-        print(final)
-    else:
-        chiral_states.append([S,E])
-
-print(len(chiral_states), len(non_chiral_states))
+# TODO: Make epsilon function more efficient
 
 def epsilon(a, b, c, d):
 
@@ -177,13 +180,17 @@ def epsilon(a, b, c, d):
 
     return summation
 
-#  Minkowski metric
 def dot(a, b):
+
+    #  Minkowski metric
+
     return a[0]*b[0] - a[1]*b[1] - a[2]*b[2] - a[3]*b[3]
 
-# a b
-# c d
 def Gram_det_2(a,b,c,d):
+
+    # a b
+    # c d
+
     return (dot(a, c))*(dot(b, d)) - (dot(a, d))*(dot(b, c))
 
 def sym_2_Gram_det(a,b):
@@ -218,19 +225,47 @@ def logic_statement_true_for_non_chiral(S, E):
                or ((sym_3_Gram_det(a,p,q) != 0) and (sym_3_Gram_det(b,p,q) != 0) and (sym_3_Gram_det(d,p,q) != 0) and (sym_3_Gram_det(c,p,q) == 0) and (Gram_det_2(a-b,RF,d,RF) == 0))
                or ((sym_3_Gram_det(a,p,q) !=0) and (sym_3_Gram_det(b,p,q) != 0) and (sym_3_Gram_det(d,p,q) != 0) and (sym_3_Gram_det(c,p,q) != 0) and (Gram_det_2(a-b,RF,c,RF) == 0) and (Gram_det_2(a-b,RF,d,RF) == 0))))
 
-    # TODO: INPUT THE CASES THAT FOLLOW BY SYMMETRY BY MAKING CASE2 A FUNCTION
+    def case_2_symmetry(p, q, a, b, c, d):
+
+        return ((dot(a,a) == dot(b,b)) and (Gram_det_2(a-b,RF,p-q,RF) == 0) and (Gram_det_2(a-b,RF,a+b,RF) == 0)
+              and (((sym_3_Gram_det(a,p,q) == 0) and (sym_3_Gram_det(b,p,q) == 0) and (sym_3_Gram_det(c,p,q) != 0) and (sym_3_Gram_det(d,p,q) == 0))
+               or ((sym_3_Gram_det(a,p,q) != 0) and (sym_3_Gram_det(b,p,q) != 0) and (sym_3_Gram_det(c,p,q) != 0) and (sym_3_Gram_det(d,p,q) == 0) and (Gram_det_2(a-b,RF,c,RF) == 0))
+               or ((sym_3_Gram_det(a,p,q) != 0) and (sym_3_Gram_det(b,p,q) != 0) and (sym_3_Gram_det(d,p,q) != 0) and (sym_3_Gram_det(c,p,q) == 0) and (Gram_det_2(a-b,RF,d,RF) == 0))
+               or ((sym_3_Gram_det(a,p,q) !=0) and (sym_3_Gram_det(b,p,q) != 0) and (sym_3_Gram_det(d,p,q) != 0) and (sym_3_Gram_det(c,p,q) != 0) and (Gram_det_2(a-b,RF,c,RF) == 0) and (Gram_det_2(a-b,RF,d,RF) == 0))))
+
+    case_3 = case_2_symmetry(p, q, c, d, a, b)  # h = (cd)
+    case_4 = case_2_symmetry(p, q, b, c, a, d)  # h = (bc)
+    case_5 = case_2_symmetry(p, q, b, d, a, c)  # h = (bd)
+    case_6 = case_2_symmetry(p, q, a, c, b, d)  # h = (ac)
+    case_7 = case_2_symmetry(p, q, a, d, c, b)  # h = (ad)
+
+    # TODO: In case 16 green and blue bracket are the negation of each other so I can evaluate once for efficiency
 
     case_16 = ((dot(a,a) == dot(b,b)) and (dot(c,c) == dot(d,d)) and (Gram_det_2(a-b,RF,p-q,RF) == 0) and (Gram_det_2(a-b,RF,a+b,RF) == 0) and (Gram_det_2(c-d,RF,p-q,RF) == 0) and (Gram_det_2(c-d,RF,c+d,RF) == 0)
               and (((sym_3_Gram_det(a,p,q)==0) and (sym_3_Gram_det(b,p,q)==0) and (sym_3_Gram_det(c,p,q)!=0) and (sym_3_Gram_det(d,p,q)!=0) or (sym_3_Gram_det(c,p,q)==0) and (sym_3_Gram_det(d,p,q)==0) and (sym_3_Gram_det(a,p,q)!=0) and (sym_3_Gram_det(b,p,q)!=0))
                    or ((Gram_det_2(a-b,RF,c+d,RF)==0) or (Gram_det_2(c-d,RF,b,RF)==0) or (Gram_det_2(a-b,RF,d,RF)==0))))
 
-    # TODO: INPUT THE CASES THAT FOLLOW BY SYMMETRY BY MAKING CASE_16 A FUNCTION
+    def case_16_symmetry(p,q,a,b,c,d):
+
+        return ((dot(a,a) == dot(b,b)) and (dot(c,c) == dot(d,d)) and (Gram_det_2(a-b,RF,p-q,RF) == 0) and (Gram_det_2(a-b,RF,a+b,RF) == 0) and (Gram_det_2(c-d,RF,p-q,RF) == 0) and (Gram_det_2(c-d,RF,c+d,RF) == 0)
+              and (((sym_3_Gram_det(a,p,q)==0) and (sym_3_Gram_det(b,p,q)==0) and (sym_3_Gram_det(c,p,q)!=0) and (sym_3_Gram_det(d,p,q)!=0) or (sym_3_Gram_det(c,p,q)==0) and (sym_3_Gram_det(d,p,q)==0) and (sym_3_Gram_det(a,p,q)!=0) and (sym_3_Gram_det(b,p,q)!=0))
+                   or ((Gram_det_2(a-b,RF,c+d,RF)==0) or (Gram_det_2(c-d,RF,b,RF)==0) or (Gram_det_2(a-b,RF,d,RF)==0))))
+
+    case_17 = case_16_symmetry(p, q, a, c, b, d)  # h = (ac)(bd)
+    case_18 = case_16_symmetry(p, q, a, d, c, b)  # h = (ad)(bc)
 
     case_19 = ((dot(a,a) == dot(b,b) == dot(c,c) == dot(d,d)) and (Gram_det_2(a-b,RF,a+b,RF) == 0) and (Gram_det_2(b-c,RF,b+c,RF) == 0) \
               and (Gram_det_2(c-d,RF,c+d,RF) == 0) and (Gram_det_2(a-b,RF,p-q,RF) == 0) and (Gram_det_2(b-c,RF,p-q,RF) == 0) \
               and (Gram_det_2(c-d,RF,p-q,RF) == 0) and (a == c) and (b == d))
 
-    # TODO: INPUT THE CASES THAT FOLLOW BY SYMMETRY BY MAKING CASE_19 A FUNCTION
+    def case_19_symmetry(p,q,a,b,c,d):
+
+        return ((dot(a,a) == dot(b,b) == dot(c,c) == dot(d,d)) and (Gram_det_2(a-b,RF,a+b,RF) == 0) and (Gram_det_2(b-c,RF,b+c,RF) == 0) \
+              and (Gram_det_2(c-d,RF,c+d,RF) == 0) and (Gram_det_2(a-b,RF,p-q,RF) == 0) and (Gram_det_2(b-c,RF,p-q,RF) == 0) \
+              and (Gram_det_2(c-d,RF,p-q,RF) == 0) and (a == c) and (b == d))
+
+    case_20 = case_19_symmetry(p,q,a,b,d,c) # h = (dbac)
+    case_22 = case_19_symmetry(p,q,a,c,d,b) # h = (bcad)
 
     case_25 = ((dot(p,p) == dot(q,q)) and (Gram_det_2(a,RF,p-q,RF) == 0) and (Gram_det_2(b,RF,p-q,RF) == 0) \
              and (Gram_det_2(c,RF,p-q,RF) == 0) and (Gram_det_2(d,RF,p-q,RF) == 0))
@@ -239,21 +274,49 @@ def logic_statement_true_for_non_chiral(S, E):
                and (Gram_det_2(c,RF,p-q,RF) == 0) and (Gram_det_2(d,RF,p-q,RF) == 0)
                and (sym_3_Gram_det(a,p,q) == 0 or sym_3_Gram_det(a+b,p,q) == 0 or sym_3_Gram_det(a-b,p,q) == 0))
 
-    # TODO: INPUT THE CASES THAT FOLLOW BY SYMMETRY BY MAKING CASE_26 A FUNCTION
+    def case_26_symmetry(p,q,a,b,c,d):
+
+        return ((dot(p,p) == dot(q,q)) and (dot(a,a) == dot(b,b)) and (Gram_det_2(a+b,RF,p-q,RF) == 0) and (Gram_det_2(a-b,RF,a+b,RF) == 0)
+               and (Gram_det_2(c,RF,p-q,RF) == 0) and (Gram_det_2(d,RF,p-q,RF) == 0)
+               and (sym_3_Gram_det(a,p,q) == 0 or sym_3_Gram_det(a+b,p,q) == 0 or sym_3_Gram_det(a-b,p,q) == 0))
+
+    case_27 = case_26_symmetry(p, q, c, d, a, b)  # h = (cd)
+    case_28 = case_26_symmetry(p, q, c, b, a, d)  # h = (bc)
+    case_29 = case_26_symmetry(p, q, d, b, c, a)  # h = (bd)
+    case_30 = case_26_symmetry(p, q, a, c, b, d)  # h = (ac)
+    case_31 = case_26_symmetry(p, q, a, d, c, b)  # h = (ad)
+
 
     case_32 = ((dot(p,p) == dot(q,q)) and (dot(b,b) == dot(c,c) == dot(d,d)) and (Gram_det_2(a,RF,p-q,RF) == 0) and (Gram_det_2(b,RF,p-q,RF) == 0) \
               and (Gram_det_2(c,RF,p-q,RF) == 0) and (Gram_det_2(d,RF,p-q,RF) == 0) and (Gram_det_2(b-c,RF,b+c,RF) == 0) \
               and (Gram_det_2(c-d,RF,c+d,RF) == 0) and (Gram_det_2(a,RF,p-q,RF) == 0) and (epsilon((b+d),c,p,q) == 0) \
               and (epsilon((b+c),d,p,q) == 0) and (epsilon((c+d),b,p,q) == 0))
 
-    # TODO: INPUT THE CASES THAT FOLLOW BY SYMMETRY BY MAKING CASE_32 A FUNCTION
+    def case_32_symmetry(p,q,a,b,c,d):
+
+        return ((dot(p,p) == dot(q,q)) and (dot(b,b) == dot(c,c) == dot(d,d)) and (Gram_det_2(a,RF,p-q,RF) == 0) and (Gram_det_2(b,RF,p-q,RF) == 0) \
+              and (Gram_det_2(c,RF,p-q,RF) == 0) and (Gram_det_2(d,RF,p-q,RF) == 0) and (Gram_det_2(b-c,RF,b+c,RF) == 0) \
+              and (Gram_det_2(c-d,RF,c+d,RF) == 0) and (Gram_det_2(a,RF,p-q,RF) == 0) and (epsilon((b+d),c,p,q) == 0) \
+              and (epsilon((b+c),d,p,q) == 0) and (epsilon((c+d),b,p,q) == 0))
+
+    case_34 = case_32_symmetry(p, q, d, b, c, a)  # h = (cba)
+    case_35 = case_32_symmetry(p, q, c, a, d, b)  # h = (dba)
+    case_37 = case_32_symmetry(p, q, b, a, c, d)  # h = (dca)
 
     case_40 = ((dot(p,p) == dot(q,q)) and (dot(a,a) == dot(b,b)) and (dot(c,c) == dot(d,d)) and (Gram_det_2(a+b,RF,p-q,RF) == 0) \
               and (Gram_det_2(c+d,RF,p-q,RF) == 0) \
               and ((sym_3_Gram_det(a+b,p,q) == 0 or sym_3_Gram_det(a-b,p,q) == 0)
                    or ((sym_3_Gram_det(c+d,p,q) == 0) or (sym_3_Gram_det(c-d,p,q) == 0))))
 
-    # TODO: INPUT THE CASES THAT FOLLOW BY SYMMETRY BY MAKING CASE_40 A FUNCTION
+    def case_40_symmetry(p,q,a,b,c,d):
+
+        return ((dot(p,p) == dot(q,q)) and (dot(a,a) == dot(b,b)) and (dot(c,c) == dot(d,d)) and (Gram_det_2(a+b,RF,p-q,RF) == 0) \
+              and (Gram_det_2(c+d,RF,p-q,RF) == 0) \
+              and ((sym_3_Gram_det(a+b,p,q) == 0 or sym_3_Gram_det(a-b,p,q) == 0)
+                   or ((sym_3_Gram_det(c+d,p,q) == 0) or (sym_3_Gram_det(c-d,p,q) == 0))))
+
+    case_41 = case_40_symmetry(p, q, a, c, b, d)  # h = (ac)(bd)
+    case_42 = case_40_symmetry(p, q, a, d, c, b)  # h = (ad)(cb)
 
     case_43 = ((dot(p,p) == dot(q,q)) and (dot(a,a) == dot(b,b) == dot(c,c) == dot(d,d)) and (Gram_det_2(a-b,RF,a+b,RF) == 0)
     and (Gram_det_2(b-c,RF,b+c,RF) == 0) and (Gram_det_2(c-d,RF,c+d,RF) == 0) and (Gram_det_2(a+b,RF,p-q,RF) == 0) and (Gram_det_2(b+c,RF,p-q,RF) == 0)
@@ -262,6 +325,37 @@ def logic_statement_true_for_non_chiral(S, E):
     or ((sym_3_Gram_det(a-c,p,q) == 0) and (sym_3_Gram_det(b-d,p,q) == 0)
         and ((sym_3_Gram_det(a+b,p,q) == 0) or ((Gram_det_2(a+b,RF,p-q,RF) == 0) and (sym_3_Gram_det(a-b,p,q) == 0))))))
 
-    # TODO: INPUT THE CASES THAT FOLLOW BY SYMMETRY BY MAKING CASE_43 A FUNCTION
+    def case_43_symmetry(p,q,a,b,c,d):
+
+        return ((dot(p,p) == dot(q,q)) and (dot(a,a) == dot(b,b) == dot(c,c) == dot(d,d)) and (Gram_det_2(a-b,RF,a+b,RF) == 0)
+    and (Gram_det_2(b-c,RF,b+c,RF) == 0) and (Gram_det_2(c-d,RF,c+d,RF) == 0) and (Gram_det_2(a+b,RF,p-q,RF) == 0) and (Gram_det_2(b+c,RF,p-q,RF) == 0)
+    and (Gram_det_2(c+d,RF,p-q,RF) == 0) and ((sym_3_Gram_det(a,p,q) == 0)
+    or ((epsilon(p+q,a-c,p,b) == 0) and (epsilon(p+q,b-d,a,p) == 0))
+    or ((sym_3_Gram_det(a-c,p,q) == 0) and (sym_3_Gram_det(b-d,p,q) == 0)
+        and ((sym_3_Gram_det(a+b,p,q) == 0) or ((Gram_det_2(a+b,RF,p-q,RF) == 0) and (sym_3_Gram_det(a-b,p,q) == 0))))))
+
+    case_44 = case_43_symmetry(p, q, a, b, d, c)  # h = (dbac)
+    case_45 = case_43_symmetry(p, q, b, a, c, d)  # h = (dcab)
+    case_46 = case_43_symmetry(p, q, a, c, b, d)  # h = (dbca)
+    case_47 = case_43_symmetry(p, q, c, b, a, d)  # h = (dabc)
+    case_48 = case_43_symmetry(p, q, b, c, a, d)  # h = (dacb)
+
+    return (case_1 or case_2 or case_3 or case_4 or case_5 or case_6 or case_7 or case_16 or case_17 or case_18
+            or case_19 or case_20 or case_22 or case_25 or case_26 or case_27 or case_28 or case_29 or case_30
+            or case_31 or case_32 or case_34 or case_35 or case_37 or case_40 or case_41 or case_42 or case_43
+            or case_44 or case_45 or case_46 or case_47 or case_48)
+
+mp,mq,ma,mb,mc,md = 1, 2, 3, 4, 5, 6
+p = uniform(-10, 10)*e3
+q = -p
+a = uniform(-10, 10)*e1 + uniform(-10, 10)*e2 + uniform(-10, 10)*e3
+b = uniform(-10, 10)*e1 + uniform(-10, 10)*e2 + uniform(-10, 10)*e3
+c = uniform(-10, 10)*e1 + uniform(-10, 10)*e2 + uniform(-10, 10)*e3
+d = uniform(-10, 10)*e1 + uniform(-10, 10)*e2 + uniform(-10, 10)*e3
+Ep,Eq,Ea,Eb,Ec,Ed = energy(mp,p), energy(mq,q), energy(ma,a), energy(mb,b), energy(mc,c), energy(md,d)
+E = [Ep,Eq,Ea,Eb,Ec,Ed]
+S = [p, q, a, b, c, d]
+
+print(logic_statement_true_for_non_chiral(S,E))
 
 # -------------------------------------------
